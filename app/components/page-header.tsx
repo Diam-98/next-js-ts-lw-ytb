013304f7356af7970b2d80a82ff23f11f511dfe4
Diam Diallo <diamil.dev@gmail.com>
2024-06-06 10:19:27 +0000

    end : header comp

--- app/components/page-header.tsx
+++ app/components/page-header.tsx
@@ -1,13 +1,29 @@
-import { Bell, MenuIcon, Mic, Search, Upload, User } from 'lucide-react'
+'use client'
+
+import {
+  ArrowLeft,
+  Bell,
+  MenuIcon,
+  Mic,
+  Search,
+  Upload,
+  User,
+} from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './button'
 
 export default function PageHeader() {
+  const [showFullWidthSearch, setShowFullWidthSearch] = useState(false)
+
   return (
     <div className='flex gap-10 lg:gap-20 justify-between  pt-2 mb-6 mx-4'>
-      <div className='flex items-center gap-4 flex-shrink-0'>
+      <div
+        className={`flex items-center gap-4 flex-shrink-0 ${
+          showFullWidthSearch ? 'hidden' : 'flex'
+        }`}
+      >
         <Button variant='ghost' size='icon'>
           <MenuIcon />
         </Button>
@@ -15,7 +31,23 @@ export default function PageHeader() {
           <Image src='/Logo.png' alt='logo' width='100' height='100' />
         </Link>
       </div>
-      <form className='flex gap-4 flex-grow justify-center'>
+      <form
+        className={`md:flex gap-4 flex-grow justify-center ${
+          showFullWidthSearch ? 'flex' : 'hidden'
+        }`}
+      >
+        {showFullWidthSearch && (
+          <Button
+            onClick={() => setShowFullWidthSearch(false)}
+            type='button'
+            size='icon'
+            variant='ghost'
+            className='flex-shrink-0'
+          >
+            <ArrowLeft />
+          </Button>
+        )}
+
         <div className='flex flex-grow max-w-[600px]'>
           <input
             type='search'
@@ -30,7 +62,22 @@ export default function PageHeader() {
           <Mic />
         </Button>
       </form>
-      <div className='flex flex-shrink-0 md:gap-2'>
+      <div
+        className={`flex flex-shrink-0 md:gap-2 ${
+          showFullWidthSearch ? 'hidden' : 'flex'
+        }`}
+      >
+        <Button
+          variant='ghost'
+          size='icon'
+          className='md:hidden'
+          onClick={() => setShowFullWidthSearch(true)}
+        >
+          <Search />
+        </Button>
+        <Button variant='ghost' size='icon' className='md:hidden'>
+          <Mic />
+        </Button>
         <Button variant='ghost' size='icon'>
           <Upload />
         </Button>
